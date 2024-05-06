68bb2f9b55b22032aa1819aaac36f7810eac86b4
Alejandro <amoralest020906@hotmail.com>
2024-05-06 09:07:49 -0600

    chore: Add API service and update dependencies

--- measurement-application/src/app/app.component.ts
+++ measurement-application/src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'measurement-application';
+export class AppComponent implements OnInit {
+  data: any;
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService.getData().subscribe((data) => {
+      this.data = data;
+      console.log(this.data);
+    });
+  }
 }
